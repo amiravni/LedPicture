1a2105e333c03bcad62a4074b0e11aaf7a890579
Amir Avni <amiravni83@gmail.com>
2014-12-20 19:01:42 +0200

    Version 1.0 Ready

--- LedPicture.ino
+++ LedPicture.ino
@@ -1,5 +1,5 @@
 #define PIN 2
-
+#define PROGS 10
 #include <Adafruit_NeoPixel.h>
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(80, PIN, NEO_GRB + NEO_KHZ800);
 
@@ -7,13 +7,16 @@ Adafruit_NeoPixel strip = Adafruit_NeoPixel(80, PIN, NEO_GRB + NEO_KHZ800);
 #include "Hebrew_Letter_patterns.h"
 #include "Unicode_Patterns.h"
 
-
-
+int colorBias=0;
+int dlyall = 50;
+int intCounter=0;
+int intflag=0;
 
 void setup() {
   Serial.begin(9600);
   strip.begin();
   strip.show(); // Initialize all pixels to 'off'
+  attachInterrupt(1, moveCounter, FALLING);
 }
 
 void loop() {
@@ -25,50 +28,149 @@ void loop() {
   BUILD_SHAPE(TAF,Taf);
   BUILD_SHAPE(HEART,Heart);
 
-  char y=0;
-  for (char x=-5; x<COL-4;x++)    
+  Resh.updateBias( COL-5, 0);
+  Vav.updateBias( COL-6, 1);
+  Taf.updateBias( COL-10, 1);
+  Heart.updateBias( 5, 4);
+
+
+  int dly = 1;
+
+  while (dlyall <175)
   {
-    Resh.updateBias(x,y);
-    Resh.updateColor(strip.Color(0, 0, 255));
-    Resh.printPattern(false);
-    Vav.updateBias(-1 + x,1 + y);
-    Vav.updateColor(strip.Color(0, 0, 255));
-    Vav.printPattern(false);   
-    Taf.updateBias(-5 + x,1 + y);
-    Taf.updateColor(strip.Color(0, 0, 255));
-    Taf.printPattern(false);  
-    Heart.updateBias( x, 4+y);
-    Heart.updateColor(strip.Color(255, 0, 0));
-    Heart.printPattern(false);  
-    strip.show();
-    delay(200);
-    matrixClear();
-  }
-
-  char x = COL-5;
-  Resh.printPattern(false);
-  Vav.printPattern(false);   
-  Taf.printPattern(false);  
-  Heart.printPattern(false);  
-  strip.show();    
-  delay(1000);
-
-  while(1) {
-    for(int j=0; j<255; j++) {
-      Resh.updateColorWheel(j);
-      Vav.updateColorWheel(j);   
-      Taf.updateColorWheel(j);  
-      Heart.updateColorFadeRed();        
-      Resh.printPattern(false);
-      Vav.printPattern(false);   
-      Taf.printPattern(false);  
-      Heart.printPattern(false);        
-
+    dlyall=dlyall*1.1;
+    for (int bias=3;bias>=0;bias--) {
+      colorBias= colorBias+16;
+      Square(bias,colorBias,Wheel( colorBias),Wheel( colorBias),Wheel( colorBias),Wheel( colorBias),false,0);
       strip.show();
-      delay(50);      
+      Square(bias,colorBias,strip.Color(0, 0, 0),strip.Color(0, 0, 0),strip.Color(0, 0, 0),strip.Color(0, 0, 0) ,false,0);
+      delay(dlyall);
     }
   }
+
+  int count=0;
+  while( count < 500) {
+    for (int bias=0;bias<1;bias++) {
+      colorBias= colorBias%256+1;
+      Square(bias,colorBias,false,0);
+      strip.show();
+    }
+    delay(5);
+    count++;
+  }
+
+  Square(0,colorBias,strip.Color(0, 0, 0),strip.Color(0, 0, 0),strip.Color(0, 0, 0),strip.Color(0, 0, 0) ,true,30);
+
+  movePatternX (&Resh, -4, COL-4, 0, strip.Color(0, 0, 255), 150); 
+  movePatternX (&Vav, -3, COL-5, 1, strip.Color(0, 0, 255), 150); 
+  movePatternX (&Taf, -8, COL-9, 1, strip.Color(0, 0, 255), 150);   
+  movePatternY (&Heart, 9, 4, 5, strip.Color(255, 0,0), 150);  
+
+  for(int j=140; j<255; j++) {
+    Resh.updateColorWheel(j);
+    Vav.updateColorWheel(j);   
+    Taf.updateColorWheel(j);  
+    Resh.printPattern(false,0);
+    Vav.printPattern(false,0);   
+    Taf.printPattern(false,0);  
+    strip.show();
+    delay(20);           
+  }
+
+  while(1) {
+    if(intCounter%PROGS==0) {
+      for(int j=0; j<255; j++) {
+        if (intflag){ 
+          intflag=0;
+          break; 
+        }
+        Resh.updateColorWheel(j);
+        Vav.updateColorWheel(j);   
+        Taf.updateColorWheel(j);  
+        Heart.updateColorFadeRed();        
+        Resh.printPattern(false,0);
+        Vav.printPattern(false,0);   
+        Taf.printPattern(false,0);  
+        Heart.printPattern(false,0);        
+
+        strip.show();
+        delay(50);      
+      }
+    }
+    else if (intCounter%PROGS==1)
+    {      
+      movePatternX (&Heart, 5, 11, 4, strip.Color(255, 0,0), 150); 
+      intCounter++;
+    }
+    else if (intCounter%PROGS==2)
+    {      
+      Resh.updateColor(strip.Color(0, 0, 255));
+      Vav.updateColor(strip.Color(0, 0, 255));   
+      Taf.updateColor(strip.Color(0, 0, 255));  
+      Resh.printPattern(true,30);
+      Vav.printPattern(true,30);   
+      Taf.printPattern(true,30);         
+    } 
+    else if (intCounter%PROGS==3)
+    {      
+      Resh.updateColor(strip.Color(0, 255, 0));
+      Vav.updateColor(strip.Color(0, 255, 0));   
+      Taf.updateColor(strip.Color(0, 255,0));  
+      Resh.printPattern(true,30);
+      Vav.printPattern(true,30);   
+      Taf.printPattern(true,30);         
+    }   
+    else if (intCounter%PROGS==4)
+    {      
+      Resh.updateColor(strip.Color(255, 0, 0));
+      Vav.updateColor(strip.Color(255, 0, 0));   
+      Taf.updateColor(strip.Color(255, 0 ,0));  
+      Resh.printPattern(true,30);
+      Vav.printPattern(true,30);   
+      Taf.printPattern(true,30);
+
+    }
+    else if (intCounter%PROGS>=5 && intCounter%PROGS<9) {
+      if (intflag){ 
+        intflag=0;
+        matrixClear();
+      }
+      colorBias= colorBias%256+1;
+      Square(intCounter%PROGS-5,colorBias,false,0);
+      strip.show();
+      delay(10);
+    }
+    else if (intCounter%PROGS==9)
+    {
+      if (intflag){ 
+        intflag=0;
+        matrixClear();
+      }      
+      movePatternX (&Resh, -4, COL-4, 0, strip.Color(0, 0, 255), 150); 
+      movePatternX (&Vav, -3, COL-5, 1, strip.Color(0, 0, 255), 150); 
+      movePatternX (&Taf, -8, COL-9, 1, strip.Color(0, 0, 255), 150);   
+      movePatternY (&Heart, 9, 4, 5, strip.Color(255, 0,0), 150);    
+      for(int j=140; j<255; j++) {
+        Resh.updateColorWheel(j);
+        Vav.updateColorWheel(j);   
+        Taf.updateColorWheel(j);  
+        Resh.printPattern(false,0);
+        Vav.printPattern(false,0);   
+        Taf.printPattern(false,0);  
+        strip.show();
+        delay(20);           
+      }      
+      intCounter++;
+    }  
+  }
 }
+
+void moveCounter() {
+  intCounter++;
+  intflag=1;
+}
+
+
 
 
 
--- Led_Patterns.h
+++ Led_Patterns.h
@@ -23,22 +23,28 @@ void matrixClear()
   strip.show();
 }
 
-void colorPixel2D(char x, char y, uint32_t c, boolean isShow)
+
+void colorPixel2D(char x, char y, uint32_t c, boolean isShow, int dly)
 {
   if (y%2) {   
     strip.setPixelColor(((int)y+1)*(COL)- (int)x-1, c);      
+ //     Serial.println(((int)y+1)*(COL)- (int)x-1);
   }
   else{   
     strip.setPixelColor((int)y*COL + (int)x, c);  
+//  Serial.println((int)y*COL + (int)x);
   }
   if (isShow) {
     strip.show();
+    delay(dly);
   }
 }
 
 // Input a value 0 to 255 to get a color value.
 // The colours are a transition r - g - b - back to r.
 uint32_t Wheel(int WheelPos) {
+  WheelPos = WheelPos%256;
+  
   if(WheelPos < 85) {
     return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
   } 
@@ -58,6 +64,53 @@ uint32_t Wheel(int WheelPos) {
 }
 
 
+
+void Square(int bias, int colorBias, uint32_t r1,uint32_t r2,uint32_t r3,uint32_t r4,boolean isShow,int dly)
+{
+  int x=bias;
+  int y=bias;
+  int i =0;
+  for (int idx=0;idx<bias;idx++)
+  i = i + 40-8*bias;
+  
+   for ( x+bias; x<COL-1-bias;x++)  {
+       colorPixel2D(x, y, r1, isShow,dly);
+   }
+   for ( y+bias; y<ROW-1-bias;y++)  {
+       colorPixel2D(x, y, r2, isShow,dly);
+   }   
+    for (x-bias; x>0+bias ;x--)  {
+      colorPixel2D(x, y, r3, isShow,dly);
+ }     
+ for (y-bias; y>0+bias ;y--)  {
+    colorPixel2D(x, y, r4, isShow,dly);
+ }        
+}
+
+
+void Square(int bias, int colorBias,boolean isShow,int dly)
+{
+  int x=bias;
+  int y=bias;
+  int i =0;
+  int wheelMult=1;
+  i = i+bias*32;
+  
+   for ( x+bias; x<COL-1-bias;x++)  {
+     colorPixel2D(x, y, Wheel( wheelMult*i+colorBias),  isShow,dly);
+   }
+   for ( y+bias; y<ROW-1-bias;y++)  {
+     colorPixel2D(x, y, Wheel( wheelMult*i+colorBias),  isShow,dly);
+   }   
+    for (x-bias; x>0+bias ;x--)  {
+     colorPixel2D(x, y, Wheel( wheelMult*i+colorBias),  isShow,dly);
+ }    
+ for (y-bias; y>0+bias ;y--)  {
+     colorPixel2D(x, y, Wheel( wheelMult*i+colorBias),  isShow,dly);
+ }        
+}
+
+
 class pattern {
 public:
   pattern(): ptrnSize(0),biasX(0),biasY(0),xVec(NULL),yVec(NULL),color(NULL),fadeDir(NULL) {}
@@ -74,12 +127,12 @@ public:
        free(fadeDir);
    }
    
-  void printPattern(boolean isShow) {
+  void printPattern(boolean isShow,int dly) {
 
     for ( char d=0;d<ptrnSize;d++)
     {
       if (xVec[d] + biasX >= 0 && xVec[d] + biasX <COL && yVec[d]+ biasY >= 0 && yVec[d]+ biasY <ROW) {
-        colorPixel2D(xVec[d] + biasX, yVec[d]+ biasY, color[d], isShow);
+        colorPixel2D(xVec[d] + biasX, yVec[d]+ biasY, color[d], isShow,dly);
       }
     }
   }      
@@ -142,8 +195,23 @@ public:
     ptrnSize = s_ptrn;
     allocateShape(s_ptrn);
     updateVec(ptrn_x_vec, ptrn_y_vec);
+    updateColor( strip.Color(0, 0, 255));
+    updateBias(0,0);
   }
- 
+  
+  void printData() 
+  {
+       for (char i=0;i<ptrnSize;i++) {
+         Serial.println( (int)xVec[i] );
+         Serial.println( (int)yVec[i] );
+         Serial.println( color[i] );
+         Serial.println( (int)biasX );
+         Serial.println( (int)biasY );
+             Serial.println("************");
+       }
+    
+  }
+
 private:
   char * xVec;
   char * yVec;
@@ -153,5 +221,31 @@ private:
   char biasY;  
   boolean * fadeDir;
 };
+
+void movePatternX (pattern* ptrn, char xStart, char xEnd, char y, uint32_t clr, int dly) { 
+  for (char x=xStart; x<xEnd;x++)    
+  {
+    ptrn->updateColor(strip.Color(0, 0, 0));
+    ptrn->printPattern(false,0);        
+    ptrn->updateBias(x,y);
+    ptrn->updateColor(clr);
+    ptrn->printPattern(false,0);
+    strip.show();
+    delay(dly);    
+  }
+}
+
+void movePatternY (pattern* ptrn, char yStart, char yEnd, char x, uint32_t clr, int dly) { 
+  for (char y=yStart; y>=yEnd;y--)    
+  {
+    ptrn->updateColor(strip.Color(0, 0, 0));
+    ptrn->printPattern(false,0);        
+    ptrn->updateBias(x,y);
+    ptrn->updateColor(clr);
+    ptrn->printPattern(false,0);
+    strip.show();
+    delay(dly);    
+  }
+}
 
 #endif
